1bba466f19ebb9d25864eaf4c5841d1334db9635
Stefan Graupner <stefan.graupner@gmail.com>
2015-06-03 14:41:24 +0200

    Reverted HomeController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,7 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-		</div>
+		<!-- TODO: Content -->
 	</div>
 </div>
 @endsection
