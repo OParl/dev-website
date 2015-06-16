81969391ee38da0b82bf92a8fde238faf7c1bb0a
Stefan Graupner <stefan.graupner@gmail.com>
2015-06-16 11:07:53 +0200

    base layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,32 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>OParl Spezifikation</title>
+<html lang="de">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        <title>OParl Spezifikation</title>
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-</head>
-<body>
-    @include ('header')
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-12 col-md-10 col-md-offset-1">
-                @yield ('content')
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    @include ('header')
+
+                    @yield('content')
+                </div>
             </div>
         </div>
-    </div>
 
-	<!-- Scripts -->
-	<script src="{{ asset('/js/lib.js') }}"></script>
-</body>
+        <!-- Scripts -->
+        <script src="{{ asset('/js/lib.js') }}"></script>
+    </body>
 </html>
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,20 +1,11 @@
-<div class="container-fluid">
+<div class="page-header">
     <div class="row">
-        <div class="col-xs-12 col-md-10 col-md-offset-1">
-            <div class="row">
-                <div class="col-xs-12 col-sm-8">
-                    <h1><span class="text-oparl">OParl</span> - Spezifikation</h1>
-                </div>
-                <div class="col-xs-12 col-sm-4">
-                    <a class="btn btn-default">
-                        Downloads
-                    </a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12">
-                    <hr/>
-                </div>
+        <div class="col-md-10">
+            <h1><span class="text-oparl">OParl</span> - Spezifikation</h1>
+        </div>
+        <div class="col-md-2">
+            <div class="text-page-header text-right">
+                <a href="#" class="btn btn-primary">Downloads</a>
             </div>
         </div>
     </div>
