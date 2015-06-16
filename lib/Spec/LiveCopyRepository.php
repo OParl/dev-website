5250203942314ae3eda1c18669c4d40b65cb449f
Stefan Graupner <stefan.graupner@gmail.com>
2015-06-16 22:44:00 +0200

    Removed ArrayAccess as there is no actual need for it.

--- lib/Spec/LiveCopyRepository.php
+++ lib/Spec/LiveCopyRepository.php
@@ -1,10 +1,9 @@
 <?php namespace OParl\Spec;
 
-use ArrayAccess;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Support\Collection;
 
-class LiveCopyRepository implements ArrayAccess
+class LiveCopyRepository
 {
   const CHAPTER_PATH = 'livecopy/';
 
@@ -35,67 +34,5 @@ class LiveCopyRepository implements ArrayAccess
     })->reduce(function (Collection $carry, Collection $headlines) {
       return $carry->merge($headlines);
     }, collect());
-  }
-
-  /**
-   * (PHP 5 &gt;= 5.0.0)<br/>
-   * Whether a offset exists
-   * @link http://php.net/manual/en/arrayaccess.offsetexists.php
-   * @param mixed $offset <p>
-   * An offset to check for.
-   * </p>
-   * @return boolean true on success or false on failure.
-   * </p>
-   * <p>
-   * The return value will be casted to boolean if non-boolean was returned.
-   */
-  public function offsetExists($offset)
-  {
-    return isset($this->chapters[$offset]);
-  }
-
-  /**
-   * (PHP 5 &gt;= 5.0.0)<br/>
-   * Offset to retrieve
-   * @link http://php.net/manual/en/arrayaccess.offsetget.php
-   * @param mixed $offset <p>
-   * The offset to retrieve.
-   * </p>
-   * @return mixed Can return all value types.
-   */
-  public function offsetGet($offset)
-  {
-    return $this->chapters[$offset];
-  }
-
-  /**
-   * (PHP 5 &gt;= 5.0.0)<br/>
-   * Offset to set
-   * @link http://php.net/manual/en/arrayaccess.offsetset.php
-   * @param mixed $offset <p>
-   * The offset to assign the value to.
-   * </p>
-   * @param mixed $value <p>
-   * The value to set.
-   * </p>
-   * @return void
-   */
-  public function offsetSet($offset, $value)
-  {
-    $this->chapters[$offset] = $value;
-  }
-
-  /**
-   * (PHP 5 &gt;= 5.0.0)<br/>
-   * Offset to unset
-   * @link http://php.net/manual/en/arrayaccess.offsetunset.php
-   * @param mixed $offset <p>
-   * The offset to unset.
-   * </p>
-   * @return void
-   */
-  public function offsetUnset($offset)
-  {
-    unset($this->chapters[$offset]);
   }
 }
