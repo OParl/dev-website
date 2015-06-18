73365821690a26a3270c25c015878b11ec9cda94
Stefan Graupner <stefan.graupner@gmail.com>
2015-06-18 13:48:42 +0200

    Add push hook handling.

--- app/Http/Controllers/HooksController.php
+++ app/Http/Controllers/HooksController.php
@@ -1,19 +1,28 @@
 <?php namespace App\Http\Controllers;
 
+use App\Jobs\UpdateLiveCopy;
+use App\Jobs\UpdateVersionHashes;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
 
 class HooksController extends Controller
 {
+  use DispatchesJobs;
+
   public function specChange(Request $request)
   {
     // GitHub update hook, load new version hashes
-    $json = $request->json();
+    $json = json_decode($request->input('payload'), true);
 
     switch ($request->header('x-github-event'))
     {
       case 'pull_request': break;
-      case 'push': break;
+      case 'push':
+        // just initiate spec and version updates
+        $this->dispatch(new UpdateLiveCopy());
+        $this->dispatch(new UpdateVersionHashes());
+        break;
 
       case 'ping':
       default:
