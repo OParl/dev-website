86720b19c38b09cb335510ce5434f2420bf95a8d
Stefan Graupner <stefan.graupner@gmail.com>
2015-06-22 20:23:06 +0200

    Altered live copy paths for testing.

--- app/Jobs/UpdateLiveCopy.php
+++ app/Jobs/UpdateLiveCopy.php
@@ -29,25 +29,28 @@ class UpdateLiveCopy extends Job implements SelfHandling, ShouldQueue
     // remove livecopy assets
     $fs->deleteDirectory(LiveCopyRepository::CHAPTER_PATH);
 
-    $files = $gh->repo()->contents()->show('OParl', 'specs', '/dokument/master', 'HEAD');
+    $user = 'lu-j';
+    $repo = 'oparl-json-schema';
+
+    $files = $gh->repo()->contents()->show($user, $repo, '/src', 'HEAD');
     foreach ($files as $file)
     {
       if (ends_with($file['name'], '.md'))
       {
         // load a chapter file
-        $data = $gh->repo()->contents()->download('OParl', 'specs', $file['path']);
+        $data = $gh->repo()->contents()->download($user, $repo, $file['path']);
         $fs->put(LiveCopyRepository::CHAPTER_PATH.$file['name'], $data);
       }
     }
 
     // load images
-    $images = $gh->repo()->contents()->show('OParl', 'specs', '/dokument/master/images', 'HEAD');
+    $images = $gh->repo()->contents()->show($user, $repo, '/src/images', 'HEAD');
     foreach ($images as $image)
     {
       // only load pngs
       if (ends_with($image['name'], '.png'))
       {
-        $data = $gh->repo()->contents()->download('OParl', 'specs', $image['path']);
+        $data = $gh->repo()->contents()->download($user, $repo, $image['path']);
         $fs->put(LiveCopyRepository::IMAGE_PATH.$image['name'], $data);
       }
     }
