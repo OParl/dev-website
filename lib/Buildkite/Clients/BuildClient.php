e5cb02eeb34f8234a852ecf0296867dd4878d381
Stefan Graupner <stefan.graupner@gmail.com>
2015-06-24 12:35:41 +0200

    Fixed naming.

--- lib/Buildkite/Clients/BuildClient.php
+++ lib/Buildkite/Clients/BuildClient.php
@@ -8,15 +8,43 @@ class BuildClient extends AbstractClient
 
   public function __construct($token, $organization)
   {
-    if (is_null($organization)) throw new BuildkiteException("\$organization must be valid.");
     parent::__construct($token, $organization);
+
+    return $this->index();
   }
 
-  public function index($organization, $project)
+  public function index($organization = null, $project = null)
+  {
+    if (!is_null($organization))
+    {
+      $this->id = $this->validateInput($organization);
+    }
+
+    if (!is_null($this->id))
+    {
+      if (!is_null($project))
+      {
+        $project = $this->validateInput($project);
+        return $this->request('GET', 'organizations/' . $this->id . '/projects/' . $project . '/builds');
+      }
+
+      return $this->request('GET', 'organizations/' . $this->id . '/builds');
+    }
+
+    return $this->request('GET', 'builds');
+  }
+
+  public function get($project, $id, $organization = null)
   {
     $organization = $this->validateInput($organization);
     $project = $this->validateInput($project);
+    $id = $this->validateInput($id, 'int');
 
-    return $this->request('GET', 'organizations/' . $organization . '/projects/' . $project . '/builds');
+    if (is_null($organization) && !is_null($this->id))
+    {
+      $organization = $this->id;
+    } else throw new BuildkiteException("Unknown organization.");
+
+    return $this->request('GET', 'organizations/' . $organization . '/projects/' . $project . '/builds/' . $id);
   }
 }
