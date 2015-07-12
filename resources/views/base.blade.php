1df19664887179fed6282738874c8859b5fd93af
Stefan Graupner <stefan.graupner@gmail.com>
2015-07-12 13:29:09 +0200

    Stylesheet fixes

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,7 +7,8 @@
 
         <title>OParl Spezifikation</title>
 
-        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
+        <link href="{{ asset('/css/lib.css') }}" rel="stylesheet" />
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
