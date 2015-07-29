b4b15c3562de8b4a7bc1395d3cffb091afaa0329
Stefan Graupner <stefan.graupner@gmail.com>
2015-07-30 00:32:11 +0200

    Basic posts output

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,10 +2,12 @@
 
 use App\Http\Requests;
 
+use App\Model\Post;
+
 class NewsController extends Controller
 {
     public function index()
     {
-      return view('base');
+      return view('news.index')->with('posts', Post::published()->paginate(15));
     }
 }
