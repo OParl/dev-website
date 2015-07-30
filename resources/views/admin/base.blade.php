360299365d5f4cbd2cbcb7907c91d7bc0c897109
Stefan Graupner <stefan.graupner@gmail.com>
2015-07-30 10:07:22 +0200

    Proof of concept admin views.

---
+++ resources/views/admin/base.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <title>OParl.org Administration</title>
+    </head>
+    <body>
+        Administration
+
+        @yield ('content')
+    </body>
+</html>
