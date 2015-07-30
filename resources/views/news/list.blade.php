031337f16e41f46bf77e24578171f655cd84bc57
Stefan Graupner <stefan.graupner@gmail.com>
2015-07-30 09:06:32 +0200

    Extracted post listing into separate template.

---
+++ resources/views/news/list.blade.php
@@ -0,0 +1,40 @@
+@forelse ($posts as $post)
+    <div>
+        <div class="meta">
+            <h2>{{ $post->title }}</h2>
+
+            <ul class="list-unstyled list-inline">
+                <li>Veröffentlicht am: {{ $post->published_at->format('d.m.Y') }}</li>
+                <li>von {{ $post->author->name }}</li>
+
+                @if ( $post->updated_at > $post->published_at)
+                    <li>{{ $post->updated_at->diffForHumans() }} zuletzt aktualisiert</li>
+                @endif
+
+                @if ($post->tags()->count() > 0)
+                    <li>
+                        Tags:
+                        <ul class="list-inline">
+                            @foreach ($post->tags as $tag)
+                                <li>
+                                    <a href="TAGURL-{{ $tag->slug }}">{{ $tag->name }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+        <div class="content">
+            {!! $post->content !!}
+        </div>
+    </div>
+@empty
+    <div class="well well-lg">
+        <p class="text-center">
+            Leider sind keine Nachrichten für diese Anzeige vorhanden.
+        </p>
+    </div>
+@endforelse
+
+{!! $posts->render() !!}
