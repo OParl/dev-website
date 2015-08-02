bc4b850e4718f7cb2076f81d6eca84c2374e1670
Stefan Graupner <stefan.graupner@gmail.com>
2015-08-02 13:02:36 +0200

    Fixed "back to front page" link

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -6,7 +6,7 @@
                 <span class="sr-only">Toggle navigation</span>
                 <span class="glyphicon glyphicon-menu-hamburger"></span>
             </button>
-            <a class="navbar-brand" href="#"><span class="text-oparl">OParl.</span></a>
+            <a class="navbar-brand" href="{{ route('news.index') }}"><span class="text-oparl">OParl.</span></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
