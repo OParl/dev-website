d97f2e5b171e1e82b5e6220704a81dd26f451f4d
Stefan Graupner <stefan.graupner@gmail.com>
2015-08-02 13:01:47 +0200

    Datepicker config

---
+++ resources/js/datetimepicker_tooltips_de.js
@@ -0,0 +1,16 @@
+var datetimepicker_tooltips_de = {
+    today: 'Heute',
+        clear: 'Auswahl löschen (Nachricht wird damit wieder zu einem Entwurf.)',
+    selectMonth: 'Monat auswählen',
+    prevMonth: 'Vorheriger Monat',
+    nextMonth: 'Nächster Monat',
+    selectYear: 'Jahr auswählen',
+    prevYear: 'Vorheriges Jahr',
+    nextYear: 'Nächstes Jahr',
+    selectDecade: 'Jahrzehnt auswählen',
+    prevDecade: 'Vorheriges Jahrzehnt',
+    nextDecade: 'Nächstes Jahrzehnt',
+    selectCentury: 'Jahrhundert auswählen',
+    prevCentury: 'Vorheriges Jahrhundert',
+    nextCentury: 'Nächstes Jahrhundert'
+};
