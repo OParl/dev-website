c6bb0100b64e6785d9be6b6c08f772e1113ff3cc
Stefan Graupner <stefan.graupner@gmail.com>
2015-10-16 15:43:00 +0200

    PSR-2

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,22 +2,22 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
-	/**
-	 * The base URL to use while testing the application.
-	 *
-	 * @var string
-	 */
-	protected $baseUrl = 'http://localhost';
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
 
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Illuminate\Foundation\Application
-	 */
-	public function createApplication()
-	{
-		$app = require __DIR__.'/../bootstrap/app.php';
-		$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-		return $app;
-	}
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+        return $app;
+    }
 }
