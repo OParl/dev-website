2a27096461ad487f5b0adf4249631e143152158a
Stefan Graupner <stefan.graupner@gmail.com>
2015-11-04 17:38:49 +0100

    integrated code climate. let the fixing of things begin.

--- app/Console/Commands/AddUserCommand.php
+++ app/Console/Commands/AddUserCommand.php
@@ -7,46 +7,47 @@ use App\Model\User;
 class AddUserCommand extends Command
 {
     /**
-   * The name and signature of the console command.
-   *
-   * @var string
-   */
-  protected $name = 'user:add';
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $name = 'user:add';
 
-  /**
-   * The console command description.
-   *
-   * @var string
-   */
-  protected $description = 'Adds a user.';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Adds a user.';
 
     protected function getOptions()
     {
         return [
-      ['name', null, InputOption::VALUE_REQUIRED, 'The user\'s name'],
-      ['email', null, InputOption::VALUE_REQUIRED, 'The user\'s email']
-    ];
+            ['name', null, InputOption::VALUE_REQUIRED, 'The user\'s name'],
+            ['email', null, InputOption::VALUE_REQUIRED, 'The user\'s email']
+        ];
     }
 
-  /**
-   * Execute the console command.
-   *
-   * @return mixed
-   */
-  public function handle()
-  {
-      $password = $this->generateRandomPassword(16);
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $password = $this->generateRandomPassword(16);
 
-      $user = User::create([
-      'name' => $this->option('name'),
-      'email' => $this->option('email'),
-      'password' => bcrypt($password)
-    ]);
+        $user = User::create([
+            'name' => $this->option('name'),
+            'email' => $this->option('email'),
+            'password' => bcrypt($password)
+        ]);
 
-      $this->info('User '.$user->name.' successfully created with ID '.$user->id);
-      $this->line('Temporary password: '.$password);
-  }
+        $this->info('User ' . $user->name . ' successfully created with ID ' . $user->id);
+        $this->line('Temporary password: ' . $password);
+    }
 
+    // TODO: extract that to a separate class
     protected function generateRandomPassword($length = 8)
     {
         $chars = "abcdefghklmnopqrstuvxyzABCDEFGHKLMNOPQRSTUVXYZ0123456789-:;,/&%(#+*";
