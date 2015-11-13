d13ff413635aaed72f893ae3d5c518b361648b3e
Stefan Graupner <stefan.graupner@gmail.com>
2015-11-13 11:14:16 +0100

    Post model factory.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -9,11 +9,28 @@
 | database. Just tell the factory how a default model should look.
 |
 */
+use Carbon\Carbon;
+use Cocur\Slugify\Slugify;
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
 $factory->define(App\Model\User::class, function (Faker\Generator $faker) {
-  return [
-    'name' => $faker->name,
-    'email' => $faker->email,
-    'password' => bcrypt(str_random(10)),
-    'remember_token' => str_random(10),
-  ];
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'password' => bcrypt(str_random(10)),
+        'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Model\Post::class, function (Faker\Generator $faker) {
+    $title = $faker->catchPhrase;
+
+    $slugify = app(Slugify::class);
+
+    return [
+        'title' => $title,
+        'slug' => $slugify->slugify($title),
+        'content' => $faker->realText(600),
+        'published_at' => $faker->dateTimeThisMonth
+    ];
 });
