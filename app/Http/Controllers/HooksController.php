e9f1eadaed7d2129cd04db8adfe31ef57a366a77
Stefan Graupner <stefan.graupner@gmail.com>
2016-02-11 19:04:46 +0100

    Syntax/formatting fixes.

--- app/Http/Controllers/HooksController.php
+++ app/Http/Controllers/HooksController.php
@@ -1,14 +1,12 @@
 <?php namespace App\Http\Controllers;
 
-use App\Model\Environment;
-use Illuminate\Contracts\Mail\Mailer;
+use App\Http\Requests\VersionUpdateRequest;
+use App\Jobs\UpdateLiveCopy;
+use App\Jobs\UpdateVersionHashes;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
-use App\Http\Requests\VersionUpdateRequest;
-use App\Jobs\UpdateLiveCopy;
-use App\Jobs\UpdateVersionHashes;
 use OParl\Spec\BuildRepository;
 use OParl\Spec\Jobs\ExtractSpecificationBuildJob;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -27,114 +25,115 @@ class HooksController extends Controller
 {
     use DispatchesJobs;
 
-  /**
-   * GitHub Spec Change Webhook
-   *
-   * This hook is called by GitHub on certain Spec repository updates.
-   * The webhook setup on GH should be the following:
-   *
-   * - Content-type: application/x-www-form-urlencoded
-   * - Secret: should match env('GITHUB_WEBHOOK_SECRET')
-   * - Events: push, pull_request
-   *
-   * (Other events may be enabled but will be ignored.)
-   *
-   * Validation of the WebHook is done via the
-   * `ValidateGitHubWebHook` middleware.
-   *
-   * On being called, the hook checks if a push or
-   * a merged pull request was signalled and updates
-   * the live copy and the version repository accordingly.
-   *
-   * @param Request $request
-   * @see \App\Http\Middleware\ValidateGitHubWebHook
-   * @return \Illuminate\Http\JsonResponse
-   **/
-  public function specChange(Request $request)
-  {
-      switch ($request->header('x-github-event')) {
-      case 'pull_request':
-        // update jobs are only necessary on PR merges
-        $json = json_decode($request->input('payload'), true);
+    /**
+     * GitHub Spec Change Webhook
+     *
+     * This hook is called by GitHub on certain Spec repository updates.
+     * The webhook setup on GH should be the following:
+     *
+     * - Content-type: application/x-www-form-urlencoded
+     * - Secret: should match env('GITHUB_WEBHOOK_SECRET')
+     * - Events: push, pull_request
+     *
+     * (Other events may be enabled but will be ignored.)
+     *
+     * Validation of the WebHook is done via the
+     * `ValidateGitHubWebHook` middleware.
+     *
+     * On being called, the hook checks if a push or
+     * a merged pull request was signalled and updates
+     * the live copy and the version repository accordingly.
+     *
+     * @param Request $request
+     * @see \App\Http\Middleware\ValidateGitHubWebHook
+     * @return \Illuminate\Http\JsonResponse
+     **/
+    public function specChange(Request $request)
+    {
+        switch ($request->header('x-github-event')) {
+            case 'pull_request':
+                // update jobs are only necessary on PR merges
+                $json = json_decode($request->input('payload'), true);
 
-        if ($json['action'] == 'closed' && $json['merged']) {
-            $this->dispatch(new UpdateLiveCopy());
-            $this->dispatch(new UpdateVersionHashes());
+                if ($json['action'] == 'closed' && $json['merged']) {
+                    $this->dispatch(new UpdateLiveCopy());
+                    $this->dispatch(new UpdateVersionHashes());
 
-            return response()->json(['result' => 'Scheduled updates.']);
+                    return response()->json(['result' => 'Scheduled updates.']);
+                }
+
+                return response()->json(['result' => 'No merge happened. Nothing to do.']);
+
+            case 'push':
+                // just initiate spec and version updates
+                $this->dispatch(new UpdateLiveCopy());
+                $this->dispatch(new UpdateVersionHashes());
+
+                return response()->json(['result' => 'Scheduled updates.']);
+
+            case 'ping':
+            default:
+                return response()->json(['result' => Inspiring::quote()]);
         }
-
-        return response()->json(['result' => 'No merge happened. Nothing to do.']);
-
-      case 'push':
-        // just initiate spec and version updates
-        $this->dispatch(new UpdateLiveCopy());
-        $this->dispatch(new UpdateVersionHashes());
-
-        return response()->json(['result' => 'Scheduled updates.']);
-
-      case 'ping':
-      default:
-        return response()->json(['result' => Inspiring::quote()]);
     }
-  }
 
-  /**
-   * Buildkite Deploy Hook
-   *
-   * This hook is hit by the Buildkite deploy process of the specification
-   * project. It is called in two different situations:
-   *
-   * Whenever an update happens to the spec, it is automatically built by
-   * Buildkite and sent to this hook
-   *
-   * @param VersionUpdateRequest $request
-   * @param Filesystem $fs
-   * @param BuildRepository $buildRepository
-   * @return \Illuminate\Http\JsonResponse
-   **/
-  public function addVersion(
-    VersionUpdateRequest $request,
-    Filesystem $fs,
-    BuildRepository $buildRepository
-  ) {
-      try {
-          $hash = $request->input('version');
-          $build = $buildRepository->getWithHash($hash);
+    /**
+     * Buildkite Deploy Hook
+     *
+     * This hook is hit by the Buildkite deploy process of the specification
+     * project. It is called in two different situations:
+     *
+     * Whenever an update happens to the spec, it is automatically built by
+     * Buildkite and sent to this hook
+     *
+     * @param VersionUpdateRequest $request
+     * @param Filesystem $fs
+     * @param BuildRepository $buildRepository
+     * @return \Illuminate\Http\JsonResponse
+     **/
+    public function addVersion(
+        VersionUpdateRequest $request,
+        Filesystem $fs,
+        BuildRepository $buildRepository
+    )
+    {
+        try {
+            $hash = $request->input('version');
+            $build = $buildRepository->getWithHash($hash);
 
-          $fs->makeDirectory('uploads/');
+            $fs->makeDirectory('uploads/');
 
-          $this->dispatch(new ExtractSpecificationBuildJob($build));
+            $this->dispatch(new ExtractSpecificationBuildJob($build));
 
-          collect($request->file())->each(function (UploadedFile $file) use ($build) {
-        $ext = $file->guessClientExtension();
+            collect($request->file())->each(function (UploadedFile $file) use ($build) {
+                $ext = $file->guessClientExtension();
 
-        if (ends_with($ext, 'gz')) {
-            $file->move($build->tar_gz_storage_path);
+                if (ends_with($ext, 'gz')) {
+                    $file->move($build->tar_gz_storage_path);
+                }
+                if (ends_with($ext, 'bz2')) {
+                    $file->move($build->tar_bz_storage_path);
+                }
+                if (ends_with($ext, 'zip')) {
+                    $file->move($build->zip_storage_path);
+                }
+            });
+
+            return response()->json([
+                'version' => $request->input('version'),
+                'success' => true
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'version' => $request->input('version'),
+                'success' => false,
+                'exception' => sprintf(
+                    '%s in %s (%d)',
+                    $e->getMessage(),
+                    basename($e->getFile()),
+                    $e->getLine()
+                )
+            ]);
         }
-        if (ends_with($ext, 'bz2')) {
-            $file->move($build->tar_bz_storage_path);
-        }
-        if (ends_with($ext, 'zip')) {
-            $file->move($build->tar_zip_storage_path);
-        }
-      });
-
-          return response()->json([
-        'version' => $request->input('version'),
-        'success' => true
-      ]);
-      } catch (\Exception $e) {
-          return response()->json([
-        'version'   => $request->input('version'),
-        'success'   => false,
-        'exception' => sprintf(
-          '%s in %s (%d)',
-          $e->getMessage(),
-          basename($e->getFile()),
-          $e->getLine()
-        )
-      ]);
-      }
-  }
+    }
 }
--- lib/Spec/BuildRepository.php
+++ lib/Spec/BuildRepository.php
@@ -11,152 +11,152 @@ use OParl\Spec\Model\SpecificationBuild;
  **/
 class BuildRepository
 {
-  /**
-   * @var Dispatcher
-   **/
-  protected $dispatcher;
+    /**
+     * @var Dispatcher
+     **/
+    protected $dispatcher;
 
-  /**
-   * @param Dispatcher $dispatcher
-   */
-  public function __construct(Dispatcher $dispatcher)
+    /**
+     * @param Dispatcher $dispatcher
+     */
+    public function __construct(Dispatcher $dispatcher)
     {
         $this->dispatcher = $dispatcher;
     }
 
-  /**
-   * @return static
-   **/
-  public function getAvailable()
+    /**
+     * @return static
+     **/
+    public function getAvailable()
     {
         return SpecificationBuild::all()->filter(function ($build) {
-      return $build->isAvailable;
-    });
+            return $build->isAvailable;
+        });
     }
 
-  /**
-   * @param int $amount
-   * @return SpecificationBuild|\Illuminate\Support\Collection
-   */
-  public function getLatest($amount = 1, $displayable = true)
-  {
-      $query = SpecificationBuild::orderBy('created_at', 'desc');
-      if ($displayable) {
-          $query = $query->whereDisplayed(true);
-      }
-
-      if ($amount == 1) {
-          return $query->first();
-      } else {
-          return $query->take($amount)->get();
-      }
-  }
-
-  /**
-   * @param SpecificationBuild $build
-   * @return bool
-   **/
-  public function isLatest(SpecificationBuild $build)
+    /**
+     * @param SpecificationBuild $build
+     * @return bool
+     **/
+    public function isLatest(SpecificationBuild $build)
     {
         return $build->created_at === $this->getLastModified();
     }
 
-  /**
-   * @return mixed
-   **/
-  public function getLastModified()
+    /**
+     * @return mixed
+     **/
+    public function getLastModified()
     {
         return $this->getLatest(1, false)->created_at;
     }
 
-  /**
-   * @param Carbon $date
-   * @return static
-   **/
-  public function getDeletableByDate(Carbon $date)
+    /**
+     * @param int $amount
+     * @return SpecificationBuild|\Illuminate\Support\Collection
+     */
+    public function getLatest($amount = 1, $displayable = true)
     {
-        return SpecificationBuild::all()->filter(function ($build) use ($date) {
-      return $build->created_at < $date;
-    });
+        $query = SpecificationBuild::orderBy('created_at', 'desc');
+        if ($displayable) {
+            $query = $query->whereDisplayed(true);
+        }
+
+        if ($amount == 1) {
+            return $query->first();
+        } else {
+            return $query->take($amount)->get();
+        }
     }
 
-  /**
-   * @param int $amount
-   * @return mixed
-   **/
-  public function getDeletableByAmount($amount = 30)
+    /**
+     * @param Carbon $date
+     * @return static
+     **/
+    public function getDeletableByDate(Carbon $date)
+    {
+        return SpecificationBuild::all()->filter(function ($build) use ($date) {
+            return $build->created_at < $date;
+        });
+    }
+
+    /**
+     * @param int $amount
+     * @return mixed
+     **/
+    public function getDeletableByAmount($amount = 30)
     {
         $amount = intval($amount);
 
         return SpecificationBuild::where('persistent', '=', false)
-      ->orderBy('created_at', 'desc')->take($amount)->get();
+            ->orderBy('created_at', 'desc')->take($amount)->get();
     }
 
-  /**
-   * @return static
-   **/
-  public function getMissing()
+    /**
+     * @param $hash
+     * @return SpecificationBuild
+     */
+    public function getWithHash($hash)
     {
-        return SpecificationBuild::all()->filter(function ($build) {
-      return !$build->isAvailable;
-    });
+        return SpecificationBuild::whereHash($hash)->first();
     }
 
-  /**
-   * @param $hash
-   * @return SpecificationBuild
-   */
-  public function getWithHash($hash)
-  {
-      return SpecificationBuild::whereHash($hash)->first();
-  }
+    /**
+     * Request a build with a shortened hash.
+     *
+     * @param $short_hash string Shortened git hash
+     * @return SpecificationBuild
+     **/
+    public function getWithShortHash($short_hash)
+    {
+        return SpecificationBuild::where('hash', '>', $short_hash)->first();
+    }
 
-  /**
-   * Request a build with a shortened hash.
-   *
-   * @param $short_hash string Shortened git hash
-   * @return SpecificationBuild
-   **/
-  public function getWithShortHash($short_hash)
-  {
-      return SpecificationBuild::where('hash', '>', $short_hash)->first();
-  }
-
-  /**
-   * Request builds by tags.
-   *
-   * @param array $tags
-   * @return SpecificationBuild|\Illuminate\Support\Collection
-   **/
-  public function getWithTags(array $tags)
+    /**
+     * Request builds by tags.
+     *
+     * @param array $tags
+     * @return SpecificationBuild|\Illuminate\Support\Collection
+     **/
+    public function getWithTags(array $tags)
     {
         // TODO: implement tags for spec versions
     }
 
-  /**
-   * Request a specific job's assets from the build system
-   *
-   * @param $hash string The git hash of the to-be-requested build
-   **/
-  public function fetchWithHash($hash)
+    /**
+     * Request a specific job's assets from the build system
+     *
+     * @param $hash string The git hash of the to-be-requested build
+     **/
+    public function fetchWithHash($hash)
     {
         $this->dispatcher->dispatch(new RequestSpecificationBuildJob($hash));
     }
 
-  /**
-   * Enqueue all missing builds and start the request job on the first one.
-   *
-   * NOTE: It is not necessary to dequeue the first missing build again
-   *       since the build job will dequeue the completed builds anyway.
-   *
-   * @return void
-   */
-  public function fetchMissing()
+    /**
+     * Enqueue all missing builds and start the request job on the first one.
+     *
+     * NOTE: It is not necessary to dequeue the first missing build again
+     *       since the build job will dequeue the completed builds anyway.
+     *
+     * @return void
+     */
+    public function fetchMissing()
     {
         $this->getMissing()->each(function (SpecificationBuild $build) {
             $build->enqueue();
         })->first(function ($build) {
             $this->dispatcher->dispatch(new RequestSpecificationBuildJob($build->hash));
+        });
+    }
+
+    /**
+     * @return static
+     **/
+    public function getMissing()
+    {
+        return SpecificationBuild::all()->filter(function ($build) {
+            return !$build->isAvailable;
         });
     }
 }
