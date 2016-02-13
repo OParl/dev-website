3aaa0aac0cbfba6518748f231433956b9e8a7eaa
Stefan Graupner <stefan.graupner@gmail.com>
2016-02-13 12:47:04 +0100

    Replaced exec() with process.

--- lib/Spec/Jobs/ExtractSpecificationBuildJob.php
+++ lib/Spec/Jobs/ExtractSpecificationBuildJob.php
@@ -1,33 +1,38 @@
 <?php namespace OParl\Spec\Jobs;
 
 use OParl\Spec\Model\SpecificationBuild;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class ExtractSpecificationBuildJob extends SpecificationJob
 {
     protected $build = null;
 
-  /**
-   * ExtractSpecificationBuildJob constructor.
-   *
-   * @param SpecificationBuild $build
-   */
-  public function __construct(SpecificationBuild $build)
-  {
-      $this->build = $build;
-  }
+    /**
+     * ExtractSpecificationBuildJob constructor.
+     *
+     * @param SpecificationBuild $build
+     */
+    public function __construct(SpecificationBuild $build)
+    {
+        $this->build = $build;
+    }
 
 
     public function handle()
     {
         $extractCmd = sprintf('tar -xvzf %s %s',
-      $this->build->tar_gz_storage_path,
-      $this->build->extracted_files_storage_path
-    );
+            $this->build->tar_gz_storage_path,
+            $this->build->extracted_files_storage_path
+        );
 
-        $returnValue = exec($extractCmd, $output);
+        $process = new Process($extractCmd);
+        $process->mustRun();
 
-        if ($returnValue !== 0) {
-            $output = implode("\n", $output);
+        try {
+            $process->run();
+        } catch (ProcessFailedException $e) {
+            $output = $process->getOutput();
             \Log::error("Specification Build archive extraction failed. Full output follows:\n\n{$output}");
         }
     }
