6e53acd3e970c1b6f5a96f857f96e0370192bf6b
Stefan Graupner <stefan.graupner@gmail.com>
2016-03-23 17:16:50 +0100

    Keyword relations.

--- lib/Server/Model/Keyword.php
+++ lib/Server/Model/Keyword.php
@@ -6,10 +6,61 @@ use Cocur\Slugify\Slugify;
 
 class Keyword extends BaseModel
 {
-    public function getNameAttribute() {
+    public function getNameAttribute()
+    {
         if (is_null($this->attributes['name'])) {
             $slugify = Slugify::create();
             $this->attributes['name'] = $slugify->slugify($this->human_name);
         }
+    }
+
+    public function agendaItems()
+    {
+        return $this->belongsToMany(Keyword::class, 'oparl_keywords_agenda_items', 'keyword_id', 'agenda_item_id');
+    }
+
+    public function bodies()
+    {
+        return $this->belongsToMany(Keyword::class, 'oparl_keywords_bodies', 'keyword_id', 'body_id');
+    }
+
+    public function consultations()
+    {
+        return $this->belongsToMany(Keyword::class, 'oparl_keywords_consultations', 'keyword_id', 'consultation_id');
+    }
+
+    public function files()
+    {
+        return $this->belongsToMany(Keyword::class, 'oparl_keywords_files', 'keyword_id', 'file_id');
+    }
+
+    public function legislativeTerms()
+    {
+        return $this->belongsToMany(Keyword::class, 'oparl_keywords_legislative_terms', 'keyword_id', 'legislative_term_id');
+    }
+
+    public function locations()
+    {
+        return $this->belongsToMany(Keyword::class, 'oparl_keywords_locations', 'keyword_id', 'location_id');
+    }
+
+    public function meetings()
+    {
+        return $this->belongsToMany(Keyword::class, 'oparl_keywords_meetings', 'keyword_id', 'meeting_id');
+    }
+
+    public function organizations()
+    {
+        return $this->belongsToMany(Keyword::class, 'oparl_keywords_organizations', 'keyword_id', 'organization_id');
+    }
+
+    public function papers()
+    {
+        return $this->belongsToMany(Keyword::class, 'oparl_keywords_papers', 'keyword_id', 'paper_id');
+    }
+
+    public function people()
+    {
+        return $this->belongsToMany(Keyword::class, 'oparl_keywords_people', 'keyword_id', 'person_id');
     }
 }
