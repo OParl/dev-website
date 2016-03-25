c09356cd3cf70f3ad2c8eb9adc0a0d645ddffe65
Stefan Graupner <stefan.graupner@gmail.com>
2016-03-25 09:46:27 +0100

    + LegislativeTerm model.

--- lib/Server/Model/Body.php
+++ lib/Server/Model/Body.php
@@ -4,6 +4,11 @@ namespace OParl\Server\Model;
 
 class Body extends BaseModel
 {
+    public function legislativeTerms()
+    {
+        return $this->hasMany(LegislativeTerm::class, 'body_id', 'id');
+    }
+
     public function keywords() {
         return $this->belongsToMany(Keyword::class, 'oparl_keywords_bodies', 'body_id', 'keyword_id');
     }
