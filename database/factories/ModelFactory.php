5e3eb4f83abb43f7187e2639052cfbda415a5cda
Stefan Graupner <stefan.graupner@gmail.com>
2016-03-28 20:35:16 +0200

    More model factory code.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -9,26 +9,64 @@
 | database. Just tell the factory how a default model should look.
 |
 */
+use Carbon\Carbon;
 use Cocur\Slugify\Slugify;
+use RomanNumber\Formatter as Romanizer;
+
+$slugify = Slugify::create();
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 $factory->define(OParl\Server\Model\System::class, function (Faker\Generator $faker) {
 
     return [
         'oparl_version' => '1.0-dev',
-        'contact_name' => $faker->name,
+        'contact_name'  => $faker->name,
         'contact_email' => $faker->email,
-        'website' => 'http://spec.oparl.org/demo'
+        'website'       => 'http://spec.oparl.org/demo'
     ];
 });
 
 $factory->define(OParl\Server\Model\Body::class, function (Faker\Generator $faker) {
+    $name = $faker->words(7);
+
     return [
+        'name'                => $name,
+        'short_name'          => $faker->colorName,
+        'website'             => $faker->url,
+        'license'             => 'CC-BY-SA 3.0',
+        'license_valid_since' => $faker->dateTimeBetween('-1 year'),
+        'oparl_since'         => Carbon::createFromDate(2016, 1, 1),
+
+        'ags' => $faker->numberBetween(100000, 169999999999),
+        'rgs' => $faker->numberBetween(100000000000, 169999999999),
+
+        'equivalent_body' => collect(
+            range(0, $faker->numberBetween(0, 5)))
+            ->map(function ($num) use ($faker) {
+                return $faker->url;
+            }),
+
+        'contact_email' => $faker->email,
+        'contact_name' => $faker->name,
+
+        'classification' => $faker->word,
+
+        // TODO: sometimes substitute these with a Location object
+        'street_address' => $faker->streetName,
+        'postal_code' => $faker->postcode,
+        'locality' => $faker->city
     ];
 });
 
 $factory->define(OParl\Server\Model\LegislativeTerm::class, function (Faker\Generator $faker) {
+    $romanizer = new Romanizer();
+    $startDate = new Carbon($faker->dateTimeThisCentury);
+
     return [
+        'name' => sprintf('%d. Wahlperiode', $romanizer->formatNumber($faker->numberBetween(10, 22))),
+
+        'start_date' => $startDate,
+        'end_date'   => $startDate->addYear($faker->numberBetween(2, 5)),
     ];
 });
 
@@ -43,12 +81,17 @@ $factory->define(OParl\Server\Model\Consultation::class, function (Faker\Generat
 });
 
 $factory->define(OParl\Server\Model\File::class, function (Faker\Generator $faker) {
+    // TODO: automatic file generation
     return [
     ];
 });
 
-$factory->define(OParl\Server\Model\Keyword::class, function (Faker\Generator $faker) {
+$factory->define(OParl\Server\Model\Keyword::class, function (Faker\Generator $faker) use ($slugify) {
+    $humanName = $faker->words(3);
+
     return [
+        'human_name' => $humanName,
+        'name'       => $slugify->slugify($humanName),
     ];
 });
 
