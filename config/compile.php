b481b4c84af68e0eba2babd9698325c35bbbcd2c
Stefan Graupner <stefan.graupner@gmail.com>
2016-07-03 14:37:30 +0200

    Removed compiled classes.

--- config/compile.php
+++ config/compile.php
@@ -14,11 +14,6 @@ return [
     */
 
     'files' => [
-
-        realpath(__DIR__.'/../app/Providers/AppServiceProvider.php'),
-        realpath(__DIR__.'/../app/Providers/EventServiceProvider.php'),
-        realpath(__DIR__.'/../app/Providers/RouteServiceProvider.php'),
-
     ],
 
     /*
