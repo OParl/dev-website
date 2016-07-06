c671693ea2d4ebf7b1d9cbc05f173c66f9f59d8e
Stefan Graupner <stefan.graupner@gmail.com>
2016-07-06 17:20:32 +0200

    js reorganization

---
+++ resources/js/modules/helpers.js
@@ -0,0 +1,15 @@
+export default {
+    prismURLHelper() {
+        // make links in code blocks clickable
+        Prism.hooks.add('wrap', function(env) {
+            if (env.type == 'string' && env.content.match(/http/))
+            {
+                var url = env.content.replace('"', '');
+                url = url.replace("'", '');
+                displayURL = env.content.replace(/\?format=html/, '');
+                env.content = "<a href=\"" + url + "\">" + displayURL + "</a>";
+            }
+        });
+    }
+}
+
