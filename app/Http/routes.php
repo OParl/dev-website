ef8ab9e7e92ad48eefbe595b1074ed0b6c180038
Stefan Graupner <stefan.graupner@gmail.com>
2016-07-09 16:36:08 +0200

    routes are now domain bound.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,72 +11,80 @@
 |
 */
 
-Route::get('/', ['uses' => 'DevelopersController@index', 'as' => 'developers.index']);
+Route::group(['domain' => config('app.url')], function () {
+    Route::get('/', ['uses' => 'DevelopersController@index', 'as' => 'developers.index']);
 
 // Specification
-Route::get('/spezifikation', ['uses' => 'SpecificationController@index', 'as' => 'specification.index']);
-Route::get('/spezifikation.md', ['uses' => 'SpecificationController@raw', 'as' => 'specification.raw']);
-Route::get('/spezifikation/images/', ['uses' => 'SpecificationController@imageIndex', 'as' => 'specification.images']);
-Route::get('/spezifikation/images/{image}', [
-    'uses' => 'SpecificationController@image',
-    'as'   => 'specification.image',
-])->where('image', '[[:print:]]+\.(png|jpg)');
+    Route::get('/spezifikation', ['uses' => 'SpecificationController@index', 'as' => 'specification.index']);
+    Route::get('/spezifikation.md', ['uses' => 'SpecificationController@raw', 'as' => 'specification.raw']);
+    Route::get('/spezifikation/images/',
+        ['uses' => 'SpecificationController@imageIndex', 'as' => 'specification.images']);
+    Route::get('/spezifikation/images/{image}', [
+        'uses' => 'SpecificationController@image',
+        'as'   => 'specification.image',
+    ])->where('image', '[[:print:]]+\.(png|jpg)');
 
-Route::get('/spezifikation/builds.json', 'SpecificationController@builds');
+    Route::get('/spezifikation/builds.json', 'SpecificationController@builds');
 
 // Downloads
-Route::pattern('downloadsExtension', '(docx|txt|pdf|odt|html|epub|zip|tar\.gz|tar\.bz2)');
-Route::pattern('downloadsVersion', '[a-z0-9]{7}');
+    Route::pattern('downloadsExtension', '(docx|txt|pdf|odt|html|epub|zip|tar\.gz|tar\.bz2)');
+    Route::pattern('downloadsVersion', '[a-z0-9]{7}');
 
-//Route::get('/downloads', ['uses' => 'DownloadsController@index', 'as' => 'downloads.index']);
-Route::get('/downloads/latest.{downloadsExtension}', [
-    'uses' => 'DownloadsController@latest',
-    'as'   => 'downloads.latest',
-]);
-Route::get('/spezifikation.{downloadsExtension}', [
-    'uses' => 'DownloadsController@latest',
-    'as'   => 'specification.download',
-]);
+    Route::get('/downloads/latest.{downloadsExtension}', [
+        'uses' => 'DownloadsController@latest',
+        'as'   => 'downloads.latest',
+    ]);
+    Route::get('/spezifikation.{downloadsExtension}', [
+        'uses' => 'DownloadsController@latest',
+        'as'   => 'specification.download',
+    ]);
 
-Route::get('/downloads/{downloadsVersion}.{downloadsExtension}', [
-    'uses' => 'DownloadsController@getFile',
-    'as'   => 'downloads.provide',
-]);
+    Route::get('/downloads/{downloadsVersion}.{downloadsExtension}', [
+        'uses' => 'DownloadsController@getFile',
+        'as'   => 'downloads.provide',
+    ]);
 
-Route::post('/downloads', ['uses' => 'DownloadsController@selectVersion', 'as' => 'downloads.select']);
-
-Route::get('/schema/{version}/{entity}.json', [
-    'uses' => 'SchemaController@getSchema',
-    'as' => 'schema.get'
-])->where('version', '(1.0|latest)')->where('entity', '[A-Za-z]+');
+    Route::post('/downloads', ['uses' => 'DownloadsController@selectVersion', 'as' => 'downloads.select']);
 
 // Hooks
-Route::get('/_hooks', function () {
-    return redirect()->route('specification.index');
+    Route::get('/_hooks', function () {
+        return redirect()->route('specification.index');
+    });
+
+    Route::get('/_hooks/spec_change', function () {
+        return redirect()->route('specification.index');
+    });
+    Route::post('/_hooks/spec_change', [
+        'uses'       => 'HooksController@specChange',
+        'as'         => 'hooks.spec',
+        'middleware' => ['hooks.github'],
+    ]);
+
+    Route::get('/_hooks/add_version', function () {
+        return redirect()->route('specification.index');
+    });
+    Route::post('/_hooks/add_version', [
+        'uses' => 'HooksController@addVersion',
+        'as'   => 'hooks.add',
+    ]);
+
+    Route::get('/_hooks/lock_version_updates', [
+        'uses' => 'HooksController@lockVersionUpdates',
+        'as'   => 'hooks.lock_vu',
+    ]);
+
+    Route::pattern('filename', '[a-z0-9]{3,8}');
+    Route::get('/demo/{filename}.pdf', ['uses' => 'DummyFileController@show', 'as' => 'dummyfile.show']);
+    Route::get('/demo/f/{filename}.pdf', ['uses' => 'DummyFileController@serve', 'as' => 'dummyfile.serve']);
 });
 
-Route::get('/_hooks/spec_change', function () {
-    return redirect()->route('specification.index');
+Route::group(['domain' => 'schema.' . config('app.url')], function () {
+    Route::get('/', function () {
+        return redirect()->route('developers.index');
+    });
+
+    Route::get('/{version}/{entity}.json', [
+        'uses' => 'SchemaController@getSchema',
+        'as'   => 'schema.get',
+    ])->where('version', '(1.0|latest)')->where('entity', '[A-Za-z]+');
 });
-Route::post('/_hooks/spec_change', [
-    'uses'       => 'HooksController@specChange',
-    'as'         => 'hooks.spec',
-    'middleware' => ['hooks.github'],
-]);
-
-Route::get('/_hooks/add_version', function () {
-    return redirect()->route('specification.index');
-});
-Route::post('/_hooks/add_version', [
-    'uses' => 'HooksController@addVersion',
-    'as'   => 'hooks.add',
-]);
-
-Route::get('/_hooks/lock_version_updates', [
-    'uses' => 'HooksController@lockVersionUpdates',
-    'as'   => 'hooks.lock_vu',
-]);
-
-Route::pattern('filename', '[a-z0-9]{3,8}');
-Route::get('/demo/{filename}.pdf', ['uses' => 'DummyFileController@show', 'as' => 'dummyfile.show']);
-Route::get('/demo/f/{filename}.pdf', ['uses' => 'DummyFileController@serve', 'as' => 'dummyfile.serve']);
