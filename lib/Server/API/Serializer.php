4877ad72d392cf9f9a0c85bf83920aeaf91c6287
Stefan Graupner <stefan.graupner@gmail.com>
2016-07-10 14:33:48 +0200

    fixed pagination

--- lib/Server/API/Serializer.php
+++ lib/Server/API/Serializer.php
@@ -2,7 +2,7 @@
 
 namespace OParl\Server\API;
 
-use League\Fractal\Resource\ResourceInterface;
+use League\Fractal\Pagination\PaginatorInterface;
 use League\Fractal\Serializer\ArraySerializer;
 
 class Serializer extends ArraySerializer
@@ -10,5 +10,38 @@ class Serializer extends ArraySerializer
     public function collection($resourceKey, array $data)
     {
         return [($resourceKey) ? $resourceKey : 'data' => $data];
+    }
+
+    public  function meta(array $meta)
+    {
+        if (!empty($meta)) {
+            return $meta[0];
+        }
+
+        return $meta;
+    }
+
+    public function paginator(PaginatorInterface $paginator)
+    {
+        $nextPage = $paginator->getUrl($paginator->getCurrentPage() + 1);
+
+        $totalPages = ceil($paginator->getTotal() / $paginator->getPerPage());
+
+        return [[
+            'pagination' => [
+                'totalElements'   => (int) $paginator->getTotal(),
+                'elementsPerPage' => (int) $paginator->getPerPage(),
+                'totalPages'      => (int) $totalPages,
+                'currentPage'     => (int) $paginator->getCurrentPage(),
+            ],
+
+            'links' => [
+                'first' => $paginator->getUrl(1),
+                'prev'  => $paginator->getUrl($paginator->getCurrentPage() - 1),
+                'self'  => $paginator->getUrl($paginator->getCurrentPage()),
+                'next'  => ($paginator->getCurrentPage() + 1 >= $paginator->getLastPage()) ? null : $nextPage,
+                'last'  => $paginator->getUrl($totalPages),
+            ],
+        ]];
     }
 }
