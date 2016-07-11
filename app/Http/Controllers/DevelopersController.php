a1965b8107e186a4c0b2d5964931feb1950a6689
Stefan Graupner <stefan.graupner@gmail.com>
2016-07-11 10:10:15 +0200

    Redirecting on production

--- app/Http/Controllers/DevelopersController.php
+++ app/Http/Controllers/DevelopersController.php
@@ -10,6 +10,12 @@ class DevelopersController extends Controller
 {
     public function index()
     {
+        // this is still in development
+        // for now redirect to spec view
+        if (config('app.env') === 'production') {
+            return redirect()->route('specification.index');
+        }
+
         return view('developers.index');
     }
 }
