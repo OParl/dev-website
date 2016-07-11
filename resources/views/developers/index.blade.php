8329cd482c8cc565f001046ed2ef0435c11edb02
Stefan Graupner <stefan.graupner@gmail.com>
2016-07-11 10:05:34 +0200

    stubbed welcome message view

--- resources/views/developers/index.blade.php
+++ resources/views/developers/index.blade.php
@@ -1,5 +1,15 @@
 @extends ('base')
 
 @section ('content')
+    <div class="col-xs-12 col-md-8 col-md-offset-2">
+        <h1>Willkommen auf dem OParl-Entwicklerportal!</h1>
+    </div>
 
+    <div class="col-xs-12 col-md-4 col-md-offset-2">
+        <h2>Was ist OParl?</h2>
+
+    </div>
+    <div class="col-xs-12 col-md-4">
+        <h2>Und hier gibt es?</h2>
+    </div>
 @stop
