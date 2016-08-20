3d8b85dd849c0ba7ab1011341a72509ee5ec2ca5
Stefan Graupner <stefan.graupner@gmail.com>
2016-08-20 20:49:51 +0200

    Minor fixes

--- lib/Spec/Commands/DeleteSpecificationBuildsCommand.php
+++ lib/Spec/Commands/DeleteSpecificationBuildsCommand.php
@@ -13,14 +13,6 @@ class DeleteSpecificationBuildsCommand extends SpecificationCommand
     protected $name = 'specification:delete';
     protected $description = 'Delete a certain amount or date-frame of specification builds.';
 
-    protected function getArguments()
-    {
-        return [
-      ['object', InputArgument::REQUIRED, 'The type of object to delete (currently only `build`)', null],
-      ['deletionMethod', InputArgument::REQUIRED, 'The method of deletion. (By date (format=yyyy-mm-dd) or amount.)', null],
-    ];
-    }
-
     public function handle(BuildRepository $buildRepository, Filesystem $fs)
     {
         if ($this->argument('object') == 'build') {
@@ -30,33 +22,52 @@ class DeleteSpecificationBuildsCommand extends SpecificationCommand
         }
     }
 
-  /**
-   * Delete SpecificationBuild(s).
-   *
-   * @param BuildRepository $buildRepository
-   * @param Filesystem $fs
-   **/
-  protected function deleteBuilds(BuildRepository $buildRepository, Filesystem $fs)
-  {
-      /* @var \Illuminate\Support\Collection $builds */
-    $builds = null;
+    /**
+     * Delete SpecificationBuild(s).
+     *
+     * @param BuildRepository $buildRepository
+     * @param Filesystem $fs
+     **/
+    protected function deleteBuilds(BuildRepository $buildRepository, Filesystem $fs)
+    {
+        /* @var \Illuminate\Support\Collection $builds */
+        $builds = null;
 
-      $method = $this->argument('deletionMethod');
+        $method = $this->argument('deletionMethod');
 
-      if (is_numeric($method)) {
-          $builds = $buildRepository->getDeletableByAmount($method);
-      } else {
-          $date = Carbon::createFromFormat('yyyy-mm-dd', $method);
-          $builds = $buildRepository->getDeletableByDate($date);
-      }
+        if (is_numeric($method)) {
+            $builds = $buildRepository->getDeletableByAmount($method);
+        } else {
+            try {
+                $date = Carbon::createFromFormat('yyyy-mm-dd', $method);
+            } catch (\Exception $e) {
+                $this->error('Failed to parse date, please adhere to the format yyyy-mm-dd');
+                return;
+            }
 
-      $builds->each(function (SpecificationBuild $build) use ($fs) {
-      if ($build->is_available) {
-          $fs->deleteDirectory($build->storage_path);
+            $builds = $buildRepository->getDeletableByDate($date);
+        }
 
-          $build->is_available = false;
-          $build->save();
-      }
-    });
-  }
+        $builds->each(function (SpecificationBuild $build) use ($fs) {
+            if ($build->is_available) {
+                $fs->deleteDirectory($build->storage_path);
+
+                $build->is_available = false;
+                $build->save();
+            }
+        });
+    }
+
+    protected function getArguments()
+    {
+        return [
+            ['object', InputArgument::REQUIRED, 'The type of object to delete (currently only `build`)', null],
+            [
+                'deletionMethod',
+                InputArgument::REQUIRED,
+                'The method of deletion. (By date (format=yyyy-mm-dd) or amount.)',
+                null,
+            ],
+        ];
+    }
 }
