d04a6928ba23085baa01cc51c9df98bc09393858
Stefan Graupner <stefan.graupner@gmail.com>
2016-08-28 16:52:17 +0200

    adjust base url

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @var string
      */
-    protected $baseUrl = 'http://dev.dev-website.dev';
+    protected $baseUrl = 'http://dev.localhost';
 
     /**
      * Creates the application.
