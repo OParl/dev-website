2c1866b738d804cff89988791f4628e61e835b02
Stefan Graupner <stefan.graupner@gmail.com>
2016-09-25 13:51:40 +0200

    minimal panel, reduced api html version

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import Helpers from './modules/helpers.js'
-
-import FTabList from './modules/ftablist.vue'
-import FTab from './modules/ftab.vue'
 
 Vue.use(VueResource);
 
@@ -13,17 +9,6 @@ var vm = new Vue({
     created() {
         Helpers.prismURLHelper();
     },
-
-    components: {
-        FTabList,
-        FTab
-    },
-
-    events: {
-        'f-tab:activate': function(ident) {
-            Prism.highlightAll();
-        }
-    }
 });
 
 window.vm = vm;
