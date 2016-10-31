c28c9248ccea66c05ed8f83a8a346931cf9a641b
Stefan Graupner <stefan.graupner@gmail.com>
2016-10-31 20:55:23 +0100

    Extended overview

--- resources/lang/de/app.php
+++ resources/lang/de/app.php
@@ -3,23 +3,29 @@
 return [
     'contact.title'       => 'Kontakt',
     'contact.info'        => 'Zur Kontaktaufnahme mit den OParl-Entwicklern gibt es verschiedene Möglichkeiten:',
-    'contact.github'      => 'Die Diskussion über die Weiterentwicklung von OParl und das Melden von Fehlern von '
-        .'OParl-Produkten findet in den Projekten unserer GitHub-Organisation statt.',
-    'contact.mailinglist' => 'OParl-Tech ist die Mailingliste auf der technische Neuheiten und '
-        .'Anfragen besprochen werden, die keinen Platz auf GitHub haben.',
+    'contact.github'      => 'Die Diskussion über die Weiterentwicklung von OParl und das Melden von Fehlern von OParl-Produkten findet in den Projekten unserer GitHub-Organisation statt.',
+    'contact.mailinglist' => 'OParl-Tech ist die Mailingliste auf der technische Neuheiten und Anfragen besprochen werden, die keinen Platz auf GitHub haben.',
     'contact.form_info'   => 'Kontaktformular',
     'contact.form'        => 'Für allgemeine Anfragen oder Anfragen, die Ihre gewerbliche Nutzung von OParl betreffen nutzen Sie bitte unser Kontaktformular.',
 
-    'developers.title'             => 'Übersicht',
+    'developers.title' => 'Übersicht',
 
     'developers.about-oparl.title' => 'Was ist OParl?',
     'developers.about-oparl.text'  => 'OParl ist ein seit Juni 2016 veröffentlichter Standard zur maschinenlesbaren Ausgabe von Daten aus sogenannten parlamentarischen Informationssystemen.',
     'developers.about-dev.title'   => 'Was finde ich hier?',
     'developers.about-dev.text'    => 'Das OParl-Entwicklerportal ist die zentrale Anlaufstelle zwischen OParl-Implentiereren, OParl-Bereitstellern und OParl-Nutzern. Hier finden Sie technische Informationen zur Schnittstelle und weiterführendes Material zu deren Einsatz und Verwendbarkeit für verschiedene Zwecke.',
 
+    'developers.demo'           => 'Einen OParl-Endpunkt der vollständig aus generierten Daten gespeist wird und somit ideal zum Testen und Demonstrieren ihrer Anwendung geeignet ist',
+    'developers.liboparl'       => 'liboparl - Die Standardbibliothek, um Anwendungen auf Basis von OParl-konformen Systemen zu entwickeln',
+    'developers.implementors'   => 'Eine Liste von aktuellen OParl-Implementieren und Informationen zu Herstellern OParl-konformer Systeme',
+    'developers.usage-examples' => 'Kleine Beispiele zum alltäglichen Nutzwert von OParl',
+
+    'developers.validator.title'     => 'Validator',
+    'developers.validator.info-text' => 'Hier können Sie Ihren OParl-Endpunkt validieren und ausführliche Informationen über die Konformität der genutzten Implementierung und über die Konsistenz der bereit gestellten Daten erhalten.',
+
     'demo.title' => 'API Demo',
 
-    'specification.title'                  => 'Spezifikation',
+    'specification.title' => 'Spezifikation',
 
     'specification.download.title'         => 'Spezifikation herunterladen',
     'specification.download.select.title'  => 'Bitte wählen Sie ein Downloadformat aus',
--- resources/views/developers/index.blade.php
+++ resources/views/developers/index.blade.php
@@ -5,14 +5,26 @@
 @stop
 
 @section ('content')
-    <div class="row">
-        <section>
-            <h2>@lang('app.developers.about-oparl.title')</h2>
-            <p class="left">@press(trans('app.developers.about-oparl.text'))</p>
-        </section>
-        <section class="row-item--shrink-2">
-            <h2>@lang('app.developers.about-dev.title')</h2>
-            <p class="left">@press(trans('app.developers.about-dev.text'))</p>
-        </section>
-    </div>
+    <section class="row">
+        <h2 class="row-item--shrink-1">@lang('app.developers.about-oparl.title')</h2>
+        <div class="row-item">
+            <p>@press(trans('app.developers.about-oparl.text'))</p>
+        </div>
+    </section>
+    <section class="row">
+        <h2 class="row-item--shrink-1">@lang('app.developers.about-dev.title')</h2>
+        <div class="row-item">
+            <p>@press(trans('app.developers.about-dev.text'))</p>
+            <p>@lang('app.developers.demo')</p>
+            <p>@lang('app.developers.liboparl')</p>
+            <p>@lang('app.developers.implementors')</p>
+            <p>@lang('app.developers.usage-examples')</p>
+        </div>
+    </section>
+    <section class="row">
+        <h2 class="row-item--shrink-1">@lang('app.developers.validator.title')</h2>
+        <div class="row-item">
+            <p>@lang('app.developers.validator.info-text')</p>
+        </div>
+    </section>
 @stop
