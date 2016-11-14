570fa319028aa55439e5b446b677ebe26a461e17
Stefan Graupner <stefan.graupner@gmail.com>
2016-11-14 19:26:04 +0100

    Removed pretty much all the obsolete spec code.

--- lib/Spec/SpecServiceProvider.php
+++ lib/Spec/SpecServiceProvider.php
@@ -14,57 +14,6 @@ class SpecServiceProvider extends IlluminateServiceProvider
      */
     public function register()
     {
-        // make sure the container transparently handles the repositories as singletons
-        $this->app->bind(LiveVersionRepository::class, LiveVersionRepository::class, true);
-        $this->app->bind(BuildRepository::class, BuildRepository::class, true);
-        $this->app->bind(SchemaRepository::class, SchemaRepository::class, true);
 
-        $this->app->bind(LiveVersionUpdater::class, function () {
-            $fs = app(Filesystem::class);
-
-            $config = config('services.repositories.spec');
-            $gitURL = sprintf('https://github.com/%s/%s.git', $config['user'], $config['repository']);
-
-            return new LiveVersionUpdater($fs, LiveVersionRepository::PATH, $gitURL);
-        });
-
-        $this->app->bind(LiveVersionBuilder::class, function () {
-            $fs = app(Filesystem::class);
-
-            return new LiveVersionBuilder($fs, LiveVersionRepository::getLiveVersionPath());
-        });
-
-        $this->app->singleton(
-            'oparl.specification.commands.delete_builds',
-            Commands\DeleteSpecificationBuildsCommand::class
-        );
-
-        $this->app->singleton(
-            'oparl.specification.commands.update_builds_gh',
-            Commands\UpdateSpecificationBuildDataFromGitHubCommand::class
-        );
-
-        $this->app->singleton(
-            'oparl.specification.commands.request_build_bk',
-            Commands\RequestSpecificationBuildCommand::class
-        );
-
-        $this->app->singleton(
-            'oparl.specification.commands.list_builds',
-            Commands\ListSpecificationBuildsCommand::class
-        );
-
-        $this->app->singleton(
-            'oparl.specification.commands.update_live_copy',
-            Commands\UpdateLiveVersionCommand::class
-        );
-
-        $this->commands([
-            'oparl.specification.commands.delete_builds',
-            'oparl.specification.commands.update_builds_gh',
-            'oparl.specification.commands.request_build_bk',
-            'oparl.specification.commands.list_builds',
-            'oparl.specification.commands.update_live_copy',
-        ]);
     }
 }
