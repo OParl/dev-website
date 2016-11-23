1c067a901b5364da94c77160a0c4b578edfccc95
Stefan Graupner <stefan.graupner@gmail.com>
2016-11-23 13:18:13 +0100

    removed deprecated code related to multiple download versions and buildkite integration

--- app/Http/Controllers/DownloadsController.php
+++ app/Http/Controllers/DownloadsController.php
@@ -23,46 +23,4 @@ class DownloadsController extends Controller
             $extension,
         ]);
     }
-
-    public function getFile($short_hash, $extension, BuildRepository $buildRepository)
-    {
-        $build = $buildRepository->getWithShortHash($short_hash);
-
-        $file = null;
-
-        if (in_array($extension, ['zip', 'tar.gz', 'tar.bz'])) {
-            $property = sprintf('%s_storage_path', str_replace('.', '_', $extension));
-
-            $file = new \SplFileInfo($build->{$property});
-        }
-
-        if (in_array($extension, ['docx', 'txt', 'pdf', 'odt', 'html', 'epub'])) {
-            $file = new \SplFileInfo($build->discoverExtractedFile($extension));
-        }
-
-        if (is_null($file) || !$file->isFile()) {
-            abort(404, 'Die angefragte Datei wurde auf diesem Server nicht gefunden.');
-        } else {
-            if ($buildRepository->isLatest($build)) {
-                $filename = $file->getBasename();
-            } else {
-                $filename = sprintf(
-                    '%s-%s.%s',
-                    $file->getBasename($file->getExtension()),
-                    $build->short_hash,
-                    $extension);
-            }
-
-            return response()->download($file, $filename);
-        }
-    }
-
-    public function selectVersion(VersionSelectRequest $request)
-    {
-        // redirect to download link
-        return redirect(null, 302)->route('downloads.provide', [
-            $request->input('version'),
-            $request->input('format'),
-        ]);
-    }
 }
