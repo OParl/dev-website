e5d918973229d6cda2c4ba6c90c715d30e6bb86f
Stefan Graupner <stefan.graupner@gmail.com>
2016-12-28 13:20:57 +0100

    Fixed tracking.

--- app/Http/Middleware/TrackAPICall.php
+++ app/Http/Middleware/TrackAPICall.php
@@ -15,14 +15,14 @@ class TrackAPICall
      */
     public function handle($request, Closure $next)
     {
-        /* @var \Illuminate\Http\Request $request */
-        $request = $next($request);
+        /* @var \Illuminate\Http\Response $response */
+        $response = $next($request);
 
         if (class_exists('\PiwikTracker')) {
             $tracker = new \PiwikTracker(config('piwik.siteId'), config('piwik.url'));
             $tracker->doTrackPageView('demoapi:' . $request->getRequestUri());
         }
 
-        return $request;
+        return $response;
     }
 }
