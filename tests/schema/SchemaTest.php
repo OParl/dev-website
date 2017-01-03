b33d9ed6e2c49b111897944e83c4c0ae3dcab8f2
Stefan Graupner <stefan.graupner@gmail.com>
2017-01-03 20:07:28 +0100

    Amend schema test

--- tests/schema/SchemaTest.php
+++ tests/schema/SchemaTest.php
@@ -7,11 +7,34 @@ class SchemaTest extends TestCase
      */
     public function testSchema($name)
     {
-        $this->route('get', 'schema.get', [$name]);
+        $this->route('get', 'schema.get', ['1.0', $name]);
         $this->assertResponseOk();
+        $this->assertJson($this->response->getContent());
 
-        $typeIdentifier = "https://schema.oparl.org/1.0/{$name}";
-        // TODO: match type on type regex
+        $this->route('get', 'schema.get', ['1.1', $name]);
+        $this->assertResponseOk();
+        $this->assertJson($this->response->getContent());
+
+        $this->route('get', 'schema.get', ['master', $name]);
+        $this->assertResponseOk();
+        $this->assertJson($this->response->getContent());
+    }
+
+    /**
+     * Since we did not have standalone schema files for all entities on 1.0
+     * this test only applies to OParl >= 1.1
+     *
+     * @dataProvider schemaNames
+     */
+    public function testType($name)
+    {
+        $this->route('get', 'schema.get', ['1.0', $name]);
+
+        $typeName = strtolower($name);
+        $regex = preg_quote("https://schema.oparl.org/1.0/{$typeName}");
+
+        //$this->assertContains($regex, $this->response->getContent());
+        $this->markTestSkipped('This test is currently not usable as our regexes are broken.');
     }
 
     public function schemaNames()
