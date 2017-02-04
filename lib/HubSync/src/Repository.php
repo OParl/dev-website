962a83987f156adffeb8590f36f59fc45aa3d88b
Stefan Graupner <stefan.graupner@gmail.com>
2017-02-04 14:07:24 +0100

    Documentation

--- lib/HubSync/src/Repository.php
+++ lib/HubSync/src/Repository.php
@@ -8,7 +8,7 @@ use Symfony\Component\Process\Process;
 /**
  * Class Repository
  *
- * This is a repository container able to sync itself with any remote.
+ * This is a git repository container able to sync itself with any remote.
  * Syncing is done using the actual git binaries thus the process running
  * this command should be allowed to use these.
  *
@@ -85,6 +85,9 @@ class Repository
         return $process->getExitCode() == 0;
     }
 
+    /**
+     * Removes the repository
+     */
     public function clean()
     {
         $this->fs->deleteDirectory($this->path);
@@ -92,6 +95,12 @@ class Repository
     }
 
     /**
+     * Get the path to a file inside the repository
+     *
+     * Will return the repository's root path if no
+     * filename was given.
+     *
+     * @param string $file path/to/a/file
      * @return string
      */
     public function getPath($file = '')
@@ -100,14 +109,10 @@ class Repository
     }
 
     /**
-     * @return string
-     */
-    public function getAbsolutePath()
-    {
-        return $this->absolutePath;
-    }
-
-    /**
+     * Get the repository name
+     *
+     * i.e. the name of the repository's root directory
+     *
      * @return string
      */
     public function getLocalName()
@@ -116,6 +121,8 @@ class Repository
     }
 
     /**
+     * Get the remote URI used for updates
+     *
      * @return string
      */
     public function getRemoteURI()
@@ -123,17 +130,53 @@ class Repository
         return $this->remoteURI;
     }
 
+    /**
+     * Get the current repo treeish
+     *
+     * Aside from the canonical revision hash, there are
+     * also other ways to uniquely address a git commit.
+     *
+     * This method will return the shortest possible
+     * string that uniquely identifies the current HEAD.
+     * Typically, that should be the name of a branch or
+     * a checked-out non-HEAD tag.
+     *
+     * @return string
+     */
     public function getCurrentTreeish()
     {
         $revParseCmd = sprintf('git -C %s rev-parse --abbrev-ref HEAD', $this->getAbsolutePath());
         return $this->synchronousProcess($revParseCmd);
     }
 
+    /**
+     * Returns the absolute path to the repository
+     *
+     * @return string
+     */
+    public function getAbsolutePath()
+    {
+        return $this->absolutePath;
+    }
+
+    /**
+     * @return string unique shortened commit hash for HEAD
+     */
     public function getCurrentHead()
     {
         return $this->getUniqueRevision('HEAD');
     }
 
+    /**
+     * Get the unique shortened commit hash for any revision
+     *
+     * In contrast to `getCurrentTreeish` this does not
+     * return a symbolic name but the familiar git commit sha1
+     * hash in it's shortened version.
+     *
+     * @param string $revision revision can be any tree-ish
+     * @return string
+     */
     public function getUniqueRevision($revision)
     {
         $revParseCmd = sprintf('git -C %s rev-parse --short %s', $this->getAbsolutePath(), $revision);
