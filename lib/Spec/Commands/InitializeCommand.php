64da656a56d98b07d4eb11006ea139226a5e7a37
Stefan Graupner <stefan.graupner@gmail.com>
2017-03-19 13:23:55 +0100

    Rename update downloadables to update downloads

--- lib/Spec/Commands/InitializeCommand.php
+++ lib/Spec/Commands/InitializeCommand.php
@@ -17,7 +17,7 @@ class InitializeCommand extends Command
         $this->call('oparl:init:schema');
 
         $this->call('oparl:update:specification');
-        $this->call('oparl:update:downloadables', ['~1.0']);
-        $this->call('oparl:update:downloadables', ['master']);
+        $this->call('oparl:update:downloads', ['~1.0']);
+        $this->call('oparl:update:downloads', ['master']);
     }
 }
