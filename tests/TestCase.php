b3328a61c1bf93e7f4ec626226179164268df8c1
Stefan Graupner <stefan.graupner@gmail.com>
2017-04-17 12:08:15 +0200

    Fix unit tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,9 @@
 <?php
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase
+use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
+
+
+class TestCase extends BaseTestCase
 {
     /**
      * The base URL to use while testing the application.
