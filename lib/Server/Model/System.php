4702afb676d267edada9733740a9ae254a9e5624
Stefan Graupner <stefan.graupner@gmail.com>
2017-04-17 16:29:17 +0200

    Some documentation

--- lib/Server/Model/System.php
+++ lib/Server/Model/System.php
@@ -7,16 +7,25 @@ namespace OParl\Server\Model;
  */
 class System extends BaseModel
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function bodies()
     {
         return $this->hasMany(Body::class, 'system_id', 'id');
     }
 
+    /**
+     * @return string
+     */
     public function getVendorAttribute()
     {
         return 'http://oparl.org';
     }
 
+    /**
+     * @return string
+     */
     public function getProductAttribute()
     {
         return route('api.index');
