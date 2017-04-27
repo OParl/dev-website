937a910d0558a7041e2b772a796ed910eb75caa1
Stefan Graupner <stefan.graupner@gmail.com>
2017-04-27 17:43:22 +0200

    Rename DeployLibOparlCommand to UpdateLibOparlCommand

--- lib/Spec/Commands/UpdateLibOParlCommand.php
+++ lib/Spec/Commands/UpdateLibOParlCommand.php
@@ -4,13 +4,15 @@ namespace OParl\Spec\Commands;
 
 use Spec\Jobs\LibOParlBuildJob;
 
-class DeployLibOParlCommand extends Command
+class UpdateLibOParlCommand extends Command
 {
     protected $signature = 'oparl:deploy:liboparl';
     protected $description = 'Deploy the latest stable liboparl on the server';
 
     public function handle()
     {
+        $this->info('Updating liboparl');
+
         $this->dispatchNow(new LibOParlBuildJob());
     }
 }
