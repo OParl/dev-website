f03cfdbfec0a38e7086d98d806dcab637e3a8c09
Stefan Graupner <stefan.graupner@gmail.com>
2017-05-02 11:10:25 +0200

    First draft for liboparl building
    THIS IS NOT PRODUCTION READY!
    
    Liboparl will be built with the prefix configured in LIBOPARL_PREFIX,
    and currently `ninja install` is being run directly from the job
    runner which would require it to gain much more capabilities than it
    should have.
    
    In the future, this will be reworked to mark the build as installable and
    use an extra, completely separate cronjob/script which will do the actual
    installation.

--- lib/Spec/Jobs/LibOParlBuildJob.php
+++ lib/Spec/Jobs/LibOParlBuildJob.php
@@ -1,26 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sgraupner
- * Date: 27.04.17
- * Time: 10:30.
- */
 
-namespace Spec\Jobs;
+namespace OParl\Spec\Jobs;
 
 use App\Jobs\Job;
 use EFrane\HubSync\Repository;
-use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Contracts\Logging\Log;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notifiable;
-use OParl\Spec\Jobs\InteractsWithRepositoryTrait;
 
 class LibOParlBuildJob extends Job implements ShouldQueue
 {
     use InteractsWithRepositoryTrait;
-    use Queueable;
     use Notifiable;
 
     protected $prefix = '';
@@ -34,5 +25,26 @@ class LibOParlBuildJob extends Job implements ShouldQueue
     {
         $repo = new Repository($fs, 'oparl_liboparl', 'https://github.com/OParl/liboparl.git');
         $repo = $this->getUpdatedHubSync($repo, $log);
+
+        $buildDir = $repo->getAbsolutePath() . '/build';
+
+        if (!is_dir($buildDir)) {
+            mkdir($buildDir);
+        }
+
+        $meson = "meson -Dbuild_valadoc=false -Dbuild_test=false --prefix={$this->prefix}";
+        if (!file_exists($buildDir . '/build.ninja')) {
+            $success = $this->runSynchronousCommand($buildDir, $meson, $output);
+
+            if (!$success) {
+                $log->error($output);
+            }
+        }
+
+        $ninja = "ninja install";
+        $success = $this->runSynchronousCommand($buildDir, $ninja, $output);
+        if (!$success) {
+            $log->error($output);
+        }
     }
 }
