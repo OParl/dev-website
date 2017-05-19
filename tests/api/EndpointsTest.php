8e04362959f1e70e2982f7c0c6f36bba9f3dce38
Stefan Graupner <stefan.graupner@gmail.com>
2017-05-19 13:24:46 +0200

    Add basic /api/endpoints test

---
+++ tests/api/EndpointsTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sgraupner
+ * Date: 19.05.17
+ * Time: 13:21
+ */
+
+
+class EndpointsTest extends TestCase
+{
+    public function testEndpointIndex()
+    {
+        $this->route('get', 'api.endpoints.index');
+        $this->assertResponseStatus(200);
+    }
+}
