c4d4248b8c76fc365f74b6e5906d0ba523f96c7e
Stefan Graupner <stefan.graupner@gmail.com>
2017-05-19 11:27:07 +0000

    Apply fixes from StyleCI

--- tests/api/EndpointsTest.php
+++ tests/api/EndpointsTest.php
@@ -3,10 +3,8 @@
  * Created by PhpStorm.
  * User: sgraupner
  * Date: 19.05.17
- * Time: 13:21
+ * Time: 13:21.
  */
-
-
 class EndpointsTest extends TestCase
 {
     public function testEndpointIndex()
