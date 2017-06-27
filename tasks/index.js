f84aea5cc6377b10f0daf4861ee945a51c4e52ee
Stefan Graupner <stefan.graupner@gmail.com>
2017-06-27 09:10:24 +0200

    Fix watch task

--- tasks/index.js
+++ tasks/index.js
@@ -5,11 +5,11 @@ import { images } from './images'
 import { scripts } from './webpack'
 import { styles } from './styles'
 
-gulp.task('watch', () => {
-    gulp.watch('./resources/assets/sass/**/*.scss', ['styles']);
-    gulp.watch('./resources/js/**/*.js', ['scripts']);
-    gulp.watch('./resources/js/**/*.vue', ['scripts']);
-});
+export function watch() {
+    gulp.watch('./resources/assets/sass/**/*.scss', styles);
+    gulp.watch('./resources/js/**/*.js', scripts);
+    gulp.watch('./resources/js/**/*.vue', scripts);
+};
 
 export const dev = gulp.series(fonts, images, scripts, styles);
 
