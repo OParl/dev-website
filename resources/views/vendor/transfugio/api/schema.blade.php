af7b650373d8abfcc3489736caf16059f19f34f8
Stefan Graupner <stefan.graupner@gmail.com>
2017-06-30 11:58:14 +0200

    Transform schema listing to table

--- resources/views/vendor/transfugio/api/schema.blade.php
+++ resources/views/vendor/transfugio/api/schema.blade.php
@@ -8,7 +8,7 @@
     <span slot="title">Mehr&hellip;</span>
     <div slot="body" c>
         <h4 >Eigenschaften</h4>
-        <table >
+        <table style="min-width: 500px; overflow: scroll">
             <thead>
                 <tr>
                     <th>Feldname</th>
@@ -16,63 +16,32 @@
                     <th>Beschreibung</th>
                 </tr>
             </thead>
-                @foreach ($documentation->getFields() as $field)
-                    <tr>
-                        <td>
-                            @if ($field->isRequired())
-                                <strong aria-label="Zwingende Eigenschaft">{{ $field->getName() }}</strong>
-                            @elseif(isset($schema['oparl:recommended']) && in_array($name, $schema['oparl:recommended']))
-                                <em aria-label="Empfohlene Eigenschaft">{{ $name }}</em>
-                            @else
-                                {{ $field->getName() }}
-                            @endif
-                        </td>
-                        <td>
-
-                        </td>
-                        <td>
-
-                        </td>
-                    </tr>
-                @endforeach
             <tbody>
-
+            @foreach ($documentation->getFields() as $field)
+                <tr>
+                    <td>
+                        @if ($field->isRequired())
+                            <strong aria-label="Zwingende Eigenschaft">{{ $field->getName() }}</strong>
+                        @elseif(isset($schema['oparl:recommended']) && in_array($name, $schema['oparl:recommended']))
+                            <em aria-label="Empfohlene Eigenschaft">{{ $name }}</em>
+                        @else
+                            {{ $field->getName() }}
+                        @endif
+                    </td>
+                    <td>
+                        @include ('transfugio::api.schema.type_or_type_list')
+                    </td>
+                    <td>
+                        @if ($field->hasDescription())
+                            <div class="text-muted small">
+                                @press($field->getDescription())
+                            </div>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
             </tbody>
         </table>
-
-        {{--
-        <dl class="properties">
-            @foreach ($documentation->getFields() as $field)
-                <dt>
-                    @if ($field->isRequired())
-                        <span class="text-danger" aria-label="Zwingende Eigenschaft">{{ $field->getName() }}</span>
-
-                    @elseif(isset($schema['oparl:recommended']) && in_array($name, $schema['oparl:recommended']))
-                        <span class="text-success" aria-label="Empfohlene Eigenschaft">{{ $name }}</span>
-
-                    @else
-                        {{ $field->getName() }}
-                    @endif
-                </dt>
-                <dd>
-                    <div class="row">
-                        <div class="col-md-offset-1 col-md-11">
-                            <div class="type">
-                                JSON-Datentyp: @include ('transfugio::api.schema.type_or_type_list')
-                            </div>
-                            @include('transfugio::api.schema.format')
-
-                            @if ($field->hasDescription())
-                                <div class="text-muted small">
-                                    @press($field->getDescription())
-                                </div>
-                            @endif
-                        </div>
-                    </div>
-                </dd>
-            @endforeach
-        </dl>
-        --}}
 
         <div class="small">
             <h4 class="text-muted">Legende</h4>
