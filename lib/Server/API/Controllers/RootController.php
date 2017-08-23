fe76d51dbb8e8ec29a0a9a176e438ef20deadd21
Stefan Graupner <stefan.graupner@gmail.com>
2017-08-23 11:25:10 +0200

    refs T46: Filter API related routes

--- lib/Server/API/Controllers/RootController.php
+++ lib/Server/API/Controllers/RootController.php
@@ -3,11 +3,18 @@
 namespace OParl\Server\API\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Routing\Route;
+use Illuminate\Routing\Router;
 
 class RootController extends Controller
 {
-    public function index()
+    public function index(Router $router)
     {
-        return redirect()->route('api.oparl.v1.system.index', ['format' => 'html']);
+        $routes = collect($router->getRoutes()->get('GET'))
+            ->filter(function (Route $route) {
+                return starts_with($route->getName(), 'api.oparl.v1');
+            });
+
+        return view('server::overview', compact('routes'));
     }
 }
