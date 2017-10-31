382ed524e7aac034253d9f5af8556b9959a126e4
Stefan Graupner <stefan.graupner@gmail.com>
2017-10-31 11:35:19 +0100

    Add Transifex config

--- resources/lang/de/common.php
+++ resources/lang/de/common.php
@@ -1,7 +1,7 @@
 <?php
 
-return [
+return array(
     'close'             => 'Schließen',
     'svg-not-supported' => 'Ihr Browser unterstützt das SVG-Format nicht, daher wird diese Grafik nicht angezeigt.',
     'table-of-contents' => 'Inhaltsverzeichnis',
-];
+);
