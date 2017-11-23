1010ec4ad38c80b5359f7389ea0cc4e279c7dfdf
Stefan Graupner <stefan.graupner@gmail.com>
2017-11-23 11:47:39 +0100

    refs #31: Configure the validation queue job

--- lib/Spec/Jobs/ValidatorRunJob.php
+++ lib/Spec/Jobs/ValidatorRunJob.php
@@ -3,7 +3,9 @@
 namespace OParl\Spec\Jobs;
 
 use App\Jobs\Job;
+use EFrane\HubSync\Repository;
 use Illuminate\Contracts\Logging\Log;
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
 class ValidatorRunJob extends Job
@@ -42,17 +44,26 @@ class ValidatorRunJob extends Job
         $this->canSaveData = $canSaveData;
     }
 
-    public function handle(Log $log)
+    public function handle(Log $log, Filesystem $fs)
     {
         $this->log = $log;
+        $log->info("Beginning Validation for {$this->endpoint}");
+
+        $validatorRepo = new Repository($fs, 'oparl_validator', '');
 
         $validatorCmd = sprintf('./validate -fjson "%s"', $this->endpoint);
-        $validator = new Process($validatorCmd);
-        $validator->start();
 
-        foreach ($validator as $type => $data) {
-            $this->handleProgress($type, $data);
-        }
+        $validator = new Process($validatorCmd);
+        $validator->setEnv([
+            'PATH' => "{$validatorRepo->getAbsolutePath()}:/usr/local/bin:/usr/bin"
+        ]);
+        $validator->setWorkingDirectory($validatorRepo->getAbsolutePath());
+
+        $log->debug("Validator working directory: {$validator->getWorkingDirectory()}");
+        $log->debug("Validator command line: {$validator->getCommandLine()}");
+        $log->debug("Validator Environment", $validator->getEnv());
+
+        $validator->run([&$this, 'handleProgress']);
     }
 
     public function handleProgress($type, $data)
@@ -65,6 +76,7 @@ class ValidatorRunJob extends Job
                 }
                 break;
             case Process::ERR:
+                $this->log->error($data);
                 break;
         }
     }
