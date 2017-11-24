0ab705e760df75b6d00cac8051a1f88dec1fa313
Stefan Graupner <stefan.graupner@gmail.com>
2017-11-25 00:44:08 +0100

    Revert demo api to show system

--- lib/Server/API/Controllers/RootController.php
+++ lib/Server/API/Controllers/RootController.php
@@ -10,11 +10,12 @@ class RootController extends Controller
 {
     public function index(Router $router)
     {
-        $routes = collect($router->getRoutes()->get('GET'))
-            ->filter(function (Route $route) {
-                return starts_with($route->getName(), 'api.oparl.v1');
-            });
-
-        return view('server::overview', compact('routes'));
+//        $routes = collect($router->getRoutes()->get('GET'))
+//            ->filter(function (Route $route) {
+//                return starts_with($route->getName(), 'api.oparl.v1');
+//            });
+//
+//        return view('server::overview', compact('routes'));
+        return redirect()->route('api.oparl.v1.system.index', ['format' => 'html']);
     }
 }
