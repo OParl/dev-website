c338f2f70e7a2ecb9cd6241d5b01f1c197b602b4
Stefan Graupner <stefan.graupner@gmail.com>
2017-11-25 00:43:41 +0100

    Fix typo in content type

--- lib/Api/Controllers/ApiController.php
+++ lib/Api/Controllers/ApiController.php
@@ -33,7 +33,7 @@ class ApiController
         $swagger = scan(base_path('lib/Api/Controllers'));
 
         return response($swagger, 200, [
-            'Content-Tyype'               => 'application/json',
+            'Content-Type'               => 'application/json',
             'Access-Control-Allow-Origin' => '*',
         ]);
     }
