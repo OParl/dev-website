8794787af7f1e9e5d42fef1fbbb30a8545f7ed3a
Stefan Graupner <stefan.graupner@gmail.com>
2017-11-29 12:26:28 +0100

    Implement new endpoint info storage system
    Endpoint info is now a little more advanced then just storing
    the information we keep in `resources/endpoints.yml`.
    We now track the endpoints' system and parts of their body information.

---
+++ app/Jobs/EndpointInfoUpdateJob.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Model\Endpoint;
+use App\Model\EndpointBody;
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Logging\Log;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\QueryException;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class EndpointInfoUpdateJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $endpointData = [];
+
+    /**
+     * Create a new job instance.
+     *
+     * @param array $endpointData
+     */
+    public function __construct(array $endpointData)
+    {
+        $this->endpointData = $endpointData;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @param Log $log
+     * @return void
+     */
+    public function handle(Log $log)
+    {
+        /** @var Endpoint $endpoint */
+        $endpoint = null;
+
+        try {
+            $endpoint = Endpoint::query()->firstOrCreate([
+                'url' => $this->endpointData['url'],
+            ]);
+        } catch (QueryException $e) {
+            $log->error("Failed to update endpoint info", $this->endpointData);
+
+            return;
+        }
+
+        // update base data
+        if (array_key_exists('title', $this->endpointData)) {
+            $endpoint->title = $this->endpointData['title'];
+        }
+
+        if (array_key_exists('description', $this->endpointData)) {
+            $endpoint->description = $this->endpointData['description'];
+        }
+
+        // get the endpoint's system
+        $guzzle = new Client();
+        $systemResponse = $guzzle->get($endpoint->url);
+        $systemJson = json_decode((string)$systemResponse->getBody(), true);
+        $endpoint->system = $systemJson;
+
+        // get the endpoint's body list and update the known endpoint bodies
+        $bodyResponse = $guzzle->get($systemJson['body']);
+        $bodyJson = json_decode((string)$bodyResponse->getBody(), true);
+
+        collect($bodyJson['data'])->each(function (array $body) use ($log, $endpoint) {
+            /** @var EndpointBody $endpointBody */
+            $endpointBody = EndpointBody::query()->firstOrCreate([
+                'endpoint_id' => $endpoint->id,
+                'oparl_id' => $body['id'],
+            ]);
+
+            try {
+                $endpointBody->name = $body['name'];
+            } catch (\ErrorException $e) {
+                $log->error("Body {$body['id']} does not seem to have a name, this is bad!", $body);
+            }
+
+            if (array_key_exists('website', $body)) {
+                $endpointBody->website = $body['website'];
+            }
+
+            $endpointBody->endpoint()->associate($endpoint);
+
+            $endpointBody->save();
+        });
+
+        $endpoint->endpoint_fetched = Carbon::now();
+
+        $endpoint->save();
+    }
+}
--- app/Jobs/ResourcesUpdateJob.php
+++ app/Jobs/ResourcesUpdateJob.php
@@ -9,18 +9,21 @@ use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Contracts\Logging\Log;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Queue\InteractsWithQueue;
 use OParl\Spec\Jobs\InteractsWithRepositoryTrait;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class ResourcesUpdateJob.
  */
 class ResourcesUpdateJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable;
+    use InteractsWithQueue, Queueable;
     use InteractsWithRepositoryTrait;
     use Notifiable;
+    use DispatchesJobs;
 
     /**
      * Create a new job instance.
@@ -40,15 +43,16 @@ class ResourcesUpdateJob implements ShouldQueue
     public function handle(Filesystem $fs, Log $log)
     {
         $repo = new Repository($fs, 'oparl_resources', 'https://github.com/OParl/resources.git');
-        $this->getUpdatedHubSync($repo, $log);
+        //$this->getUpdatedHubSync($repo, $log);
 
         // TODO: validate endpoints.yml
         // $this->notify(SpecificationUpdateNotification::resourcesUpdateFailedNotification($this->treeish));
 
-        $fs->delete('live/endpoints.yml');
-        $fs->copy($repo->getPath('endpoints.yml'), 'live/endpoints.yml');
+        collect(Yaml::parse($fs->get($repo->getPath('endpoints.yml'))))->map(function (array $endpoint) {
+            $this->dispatch(new EndpointInfoUpdateJob($endpoint));
+        });
 
-        $this->notify(SpecificationUpdateNotification::resourcesUpdateSuccesfulNotification($repo->getCurrentHead()));
+        //$this->notify(SpecificationUpdateNotification::resourcesUpdateSuccesfulNotification($repo->getCurrentHead()));
     }
 
     /**
