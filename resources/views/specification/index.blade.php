06b899ede316d9ae2fcb7cf68dd5c9df1f0b5cef
eFrane <efrane@meanderingsoul.com>
2018-06-28 11:12:21 +0200

    Collapsible toc

--- resources/views/specification/index.blade.php
+++ resources/views/specification/index.blade.php
@@ -9,10 +9,17 @@
 @section ('content')
     <div class="columns">
         <aside class="column is-one-third">
-            <div class="box table-of-contents">
-                <strong>@lang('common.table-of-contents')</strong>
-                {!! $liveView->getTableOfContents() !!}
-            </div>
+            <b-collapse class="card">
+                <div class="card-header" slot="trigger" slot-scope="props">
+                    <strong class="card-header-title">@lang('common.table-of-contents')</strong>
+                    <div class="card-header-icon">
+                        <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
+                    </div>
+                </div>
+                <div class="card-content">
+                    {!! $liveView->getTableOfContents() !!}
+                </div>
+            </b-collapse>
         </aside>
         <main class="column is-two-thirds">
             {!! $liveView->getBody() !!}
