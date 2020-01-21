b50e17a808ef929f247474a6716e477cfc03d32f
eFrane <efrane@meanderingsoul.com>
2020-01-21 17:31:45 +0100

    Fold version selector into table of contents

--- resources/views/specification/index.blade.php
+++ resources/views/specification/index.blade.php
@@ -1,20 +1,5 @@
 @extends ('base')
 
-@section('header_extra')
-    <nav class="level u-pt-0_5">
-        <div class="level-left">&nbsp;</div>
-        <div class="level-right">
-            <div class="level-item">
-                <version-selector
-                        :versions="{{ json_encode(array_keys(config('oparl.versions.specification'))) }}"
-                        :current="liveView.currentVersion.human"
-                        @version-change="changeLiveView"
-                ></version-selector>
-            </div>
-        </div>
-    </nav>
-@stop
-
 @section ('content')
     <div class="columns">
         <aside class="column is-one-third" id="toc-container">
@@ -22,6 +7,13 @@
                 <div class="card">
                     <div class="card-header">
                         <strong class="card-header-title">Inhaltsverzeichnis</strong>
+                        <div class="card-header-icon">
+                            <version-selector
+                                    :versions="{{ json_encode(array_keys(config('oparl.versions.specification'))) }}"
+                                    :current="liveView.currentVersion.human"
+                                    @version-change="changeLiveView"
+                            ></version-selector>
+                        </div>
                     </div>
                     <div class="card-content table-of-contents" v-html="liveView.toc" v-if="!liveView.isLoading"></div>
                 </div>
