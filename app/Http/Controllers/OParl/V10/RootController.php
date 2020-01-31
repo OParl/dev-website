418f371f0a5fc64cacb21b973ddf6dace14ae8f8
eFrane <efrane@meanderingsoul.com>
2020-01-31 21:56:47 +0100

    Remove unused template
    ... and some dead code. Once the demoserver comes
    back in a functional way this may be revived.

--- app/Http/Controllers/OParl/V10/RootController.php
+++ app/Http/Controllers/OParl/V10/RootController.php
@@ -3,18 +3,11 @@
 namespace App\Http\Controllers\OParl\V10;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Routing\Router;
 
 class RootController extends Controller
 {
-    public function index(Router $router)
+    public function index()
     {
-//        $routes = collect($router->getRoutes()->get('GET'))
-//            ->filter(function (Route $route) {
-//                return starts_with($route->getName(), 'api.oparl.v1');
-//            });
-//
-//        return view('server::overview', compact('routes'));
         return redirect()->route('api.oparl.v1.system.index', ['format' => 'html']);
     }
 }
--- lib/Server/resources/views/overview.blade.php
+++
@@ -1,9 +0,0 @@
-@extends ('base')
-
-@section ('content')
-    <ul>
-        @foreach ($routes as $route)
-            <li>{{ $route->getName() }}</li>
-        @endforeach
-    </ul>
-@stop
