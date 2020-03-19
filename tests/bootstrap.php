44efff24e6f994dc1dbb7f30ddc068c128963b11
eFrane <efrane@meanderingsoul.com>
2020-03-19 11:19:53 +0100

    Adjust tests to work with PHPUnit 8.5

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,10 @@
 
 include __DIR__.'/../vendor/autoload.php';
 
+function remove_test_repo() {
+    // remove test repo
+    $cmd = sprintf('rm -rf %s', __DIR__.'/../storage/app/hub_sync/test');
+    exec($cmd);
+}
+
+remove_test_repo();
