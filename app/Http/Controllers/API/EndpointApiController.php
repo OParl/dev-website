b54db058bd9888028d2ede8d61d9ac4786653ed7
eFrane <efrane@meanderingsoul.com>
2020-04-20 13:00:49 +0200

    Add self links

--- app/Http/Controllers/API/EndpointApiController.php
+++ app/Http/Controllers/API/EndpointApiController.php
@@ -74,6 +74,7 @@ class EndpointApiController
                 'meta' => [
                     'page'  => $page,
                     'total' => $pageCount,
+                    'self' => route('api.endpoints.index', ['page' => $page, 'limit' => $limit]),
                     'next'  => ($pageCount > $page)
                         ? route('api.endpoints.index', ['page' => ++$page, 'limit' => $limit])
                         : null,
