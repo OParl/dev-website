27335d23ad761d4411b955eea25b58009d9374f6
eFrane <efrane@meanderingsoul.com>
2020-04-25 14:26:16 +0200

    Fix missing header on api index page

--- resources/js/bundles/api.js
+++ resources/js/bundles/api.js
@@ -1,5 +1,10 @@
 import OpenApi from '../modules/OpenApi.vue'
+import Buefy from 'buefy'
 import Vue from 'vue'
+
+Vue.use(Buefy, {
+  defaultContainerElement: '#app'
+});
 
 new Vue({
   el: '#app',
