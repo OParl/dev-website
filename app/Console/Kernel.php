9554d36670f5dc26cf93915973feb5f1b6df23dd
eFrane <efrane@meanderingsoul.com>
2020-05-01 11:18:03 +0200

    Remove obsolete TODO
    The accumulating nature of spec builds has long since been eliminated.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,9 +10,6 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
-        // TODO: Re-add a cleanup command for outdated spec builds
-        //$schedule->command(DeleteSpecificationBuildsCommand::class, ['build', 30])->daily();
-
         $schedule->command(ResourcesUpdateJob::class)->saturdays();
     }
 
